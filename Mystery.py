4f9b78171c40af7c68d23c673a37e5b76f392efb
Kaito Sinclaire <mattwcstrfan@gmail.com>
2020-01-06 22:13:51 -0800

    Fix Mystery.py

--- Mystery.py
+++ Mystery.py
@@ -43,7 +43,7 @@ def main():
     parser.add_argument('--rom')
     parser.add_argument('--enemizercli')
     parser.add_argument('--outputpath')
-    parser.add_argument('--extendedmsu', default=defval(False), action='store_true', help='Use Extended MSU-1 tracks.')
+    parser.add_argument('--extendedmsu', action='store_true', help='Use Extended MSU-1 tracks.')
 
     for player in range(1, multiargs.multi + 1):
         parser.add_argument(f'--p{player}', help=argparse.SUPPRESS)
